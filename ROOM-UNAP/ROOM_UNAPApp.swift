ae4a2bf7a22320cfbb05053fa326d3b7bf921e9a
Therick75 <75865979@est.unap.edu.pe>
2026-07-23 21:32:01 -0500

    neuvo commit

---
+++ ROOM-UNAP/ROOM_UNAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROOM_UNAPApp.swift
+//  ROOM-UNAP
+//
+//  Created by Erick on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ROOM_UNAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
